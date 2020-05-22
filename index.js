0a6a54a28228515a28f9967f9ea74c11c548419e
Adil Baaj <adil.baaj@nimbleways.com>
2020-05-22 01:08:46 +0000

    Use constants for drug names

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import * as drugNames from "./constants";
 
 import fs from "fs";
 
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug(drugNames.DOLIPRANE, 20, 30),
+  new Drug(drugNames.HERBALTEA, 10, 5),
+  new Drug(drugNames.FERVEX, 5, 40),
+  new Drug(drugNames.MAGICPILL, 15, 40)
 ];
 const trial = new Pharmacy(drugs);
 
